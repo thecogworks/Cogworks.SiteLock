f9a25c153aa0f3a21e910a9f9aad0037dd21326b
Sam Gooch <sam@samgooch.co.uk>
2020-10-02 16:22:20 +0100

    Add cloudflare support

---
+++ src/Cogworks.SiteLock/Web/Extensions/RequestExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+
+namespace Cogworks.SiteLock.Web.Extensions
+{
+    internal static class RequestExtensions
+    {
+        public static string GetIpAddress(this HttpRequestBase request)
+        {
+            string ipAddress = request.ServerVariables["CF-Connecting-IP"];
+            if (string.IsNullOrWhiteSpace(ipAddress))
+                ipAddress = request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                string[] addresses = ipAddress.Split(',');
+                if (addresses.Length != 0)
+                {
+                    return addresses[0];
+                }
+            }
+
+            return request.UserHostAddress;
+        }
+    }
+}
